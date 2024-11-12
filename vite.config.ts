d4fd27edbff7df38bbb4cf5e30226fdb22b8d5f5
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-11-12 09:02:16 -0300

    chore: improve and manage eslint configs and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -8,4 +9,21 @@ export default defineConfig({
     open: true,
   },
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyComponentLibrary',
+      formats: ['es', 'umd'],
+      fileName: (format) => `my-component-library.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 });
